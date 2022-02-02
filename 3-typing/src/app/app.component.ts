56d4e36e0d453e5991aeaf6bf9e46a72944e34c9
poojithullasa <ullasa.sindhur@gslab.com>
2022-02-02 22:10:50 +0530

    Completed exercise 4 Building a typing game

---
+++ 3-typing/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import faker from "@faker-js/faker";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  randomText = faker.lorem.sentence();
+  enteredText = "";
+  onInput(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return "pending";
+    }
+    return randomLetter === enteredLetter ? "correct" : "incorrect";
+  }
+}
